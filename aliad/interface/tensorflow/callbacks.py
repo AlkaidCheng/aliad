de3788a4d6bedc1c76b5de13668e94ea2de0ca27
AlkaidCheng <lung9675420@gmail.com>
2023-12-06 13:47:38 -0800

    implements a metrics logger for saving training/test metrics of a tensorflow model

--- aliad/interface/tensorflow/callbacks.py
+++ aliad/interface/tensorflow/callbacks.py
@@ -1,13 +1,8 @@
-from typing import Any, Optional
+import os
+import json
+from typing import Any, Optional, Union
 
-if sys.version_info >= (3, 8):
-    from typing import Literal
-else:
-    from typing_extensions import Literal
-    
 import tensorflow as tf
-
-
 
 class LearningRateScheduler(tf.keras.callbacks.Callback):
     """
@@ -64,18 +59,36 @@ class BatchMetricsCallback(tf.keras.callbacks.Callback):
             
 class MetricsLogger(tf.keras.callbacks.Callback):
 
+    """
+    A TensorFlow Keras callback to log and save training and testing metrics.
+
+    Provides detailed logs of metrics for each epoch and batch during training 
+    and evaluation of a TensorFlow model.
+
+    Parameters:
+        filepath (str): Directory where metrics log files will be saved. Defaults to './logs'.
+        save_freq (Union[str, int]): Determines the frequency of saving logged metrics. Defaults to -1.
+            - If 'epoch', saves epoch-level metrics at the end of each epoch.
+            - If 'batch', saves batch-level metrics after every training/testing batch.
+            - If a positive integer, saves accumulated batch-level metrics at this interval.
+            - If a negative integer, saves accumulated batch-level metrics over all batches at the end of each epoch.
+    """
+    
+    TRAIN = 'train'
+    TEST = 'test'
+
     def __init__(
         self,
         filepath: str = './logs',
-        save_freq: Union[str, int] = "epoch",
+        save_freq: Union[str, int] = -1,
         *args: Any,
         **kwargs: Any
     ) -> None:
         super().__init__(*args, **kwargs)
 
         if save_freq == "batch":
-            save_freq = -1
-        if （save_freq != "epoch") and not isinstance(save_freq, int):
+            save_freq = 1
+        if (save_freq != "epoch") and not isinstance(save_freq, int):
             raise ValueError('save_freq must be "epoch", "batch" or an integer')
 
         self.save_batch = isinstance(save_freq, int)
@@ -91,59 +104,85 @@ class MetricsLogger(tf.keras.callbacks.Callback):
             self._current_batch[stage] = 0
             self._batch_logs[stage] = []
 
-    def on_train_begin(self, logs=None):
-        os.makedirs(self.filepath, exist_ok=True)
-        os.makedirs(self.get_epoch_metrics_savedir(), exist_ok=True)
-        if self.log_batch:
-            os.makedirs(self.get_batch_metrics_savedir(), exist_ok=True)
-            self.reset_batch_data()
+    def reset_batch_data(self):
+        """Resets the batch data storage for a new epoch."""
+        self._current_batch = {self.TRAIN: 0, self.TEST: 0}
+        self._batch_logs = {self.TRAIN: [], self.TEST: []}
 
     def get_epoch_metrics_savedir(self):
+        """Returns the directory path for saving epoch metrics."""
         return os.path.join(self.filepath, "epoch_metrics")
 
     def get_batch_metrics_savedir(self):
+        """Returns the directory path for saving batch metrics."""
         return os.path.join(self.filepath, "batch_metrics")
 
-    def on_epoch_begin(self, epoch, logs=None):
-        self._current_epoch = epoch
-
-    def on_epoch_end(self, epoch, logs=None):
-        self._log_epoch(epoch, logs)
-        if self.log_batch:
-            for stage, batch_logs in self._batch_logs.items():
-                if batch_logs:
-                    self._save_metrics(batch_logs, stage=stage)
-            self.reset_batch_data()
-
     def _log_epoch(self, epoch, logs):
+        """Logs epoch-level metrics."""
         logs = dict() if logs is None else dict(logs)
         logs["epoch"] = epoch
         self._save_metrics(logs, stage='epoch')
 
     def _log_batch(self, batch, logs, stage: str):
+        """Logs batch-level metrics."""
+        if not self.save_batch:
+            return
         logs = dict() if logs is None else dict(logs)
         logs["epoch"] = self._current_epoch
         logs["batch"] = batch
         
         self._batch_logs[stage].append(logs)
 
-        if self.log_batch and (batch > 0) and ((batch + 1) % self.save_freq == 0):
+        if (self.save_freq > 0) and ((batch + 1) % self.save_freq == 0):
             self._save_metrics(self._batch_logs[stage], stage=stage)
             self._batch_logs[stage] = []
             
+    def on_train_begin(self, logs=None):
+        """Sets up directories and data at the start of training."""
+        os.makedirs(self.filepath, exist_ok=True)
+        os.makedirs(self.get_epoch_metrics_savedir(), exist_ok=True)
+        if self.save_batch:
+            os.makedirs(self.get_batch_metrics_savedir(), exist_ok=True)
+            self.reset_batch_data()
+
+    def on_epoch_begin(self, epoch, logs=None):
+        """Updates the current epoch index at the start of each epoch."""
+        self._current_epoch = epoch
+
     def on_train_batch_begin(self, batch, logs=None):
-        self._current_batch['train'] = batch
-        
-    def on_train_batch_end(self, batch, logs=None):
-        self._log_batch(batch, logs, 'train')
+        """Updates the current batch index for training at the beginning of each batch."""
+        self._current_batch[self.TRAIN] = batch
 
     def on_test_batch_begin(self, batch, logs=None):
-        self._current_batch['test'] = batch
-    
+        """Updates the current batch index for testing at the beginning of each batch."""
+        self._current_batch[self.TEST] = batch
+
+    def on_epoch_end(self, epoch, logs=None):
+        """Logs and saves metrics at the end of each epoch."""
+        self._log_epoch(epoch, logs)
+        if self.save_batch:
+            for stage, batch_logs in self._batch_logs.items():
+                if batch_logs:
+                    self._save_metrics(batch_logs, stage=stage)
+            self.reset_batch_data()
+        
+    def on_train_batch_end(self, batch, logs=None):
+        """Logs metrics at the end of each training batch."""
+        self._log_batch(batch, logs, self.TRAIN)
+
     def on_test_batch_end(self, batch, logs=None):
-        self._log_batch(batch, logs, 'test')
+        """Logs metrics at the end of each testing batch."""
+        self._log_batch(batch, logs, self.TEST)
 
     def _save_metrics(self, logs, stage=None, indent: int = 2):
+        """
+        Saves the metrics to a file.
+
+        Args:
+            logs (dict or list): Metrics to be saved.
+            stage (str, optional): The training stage ('train', 'test' or 'epoch').
+            indent (int): Indentation level for pretty-printing the JSON file.
+        """
         if not logs:
             return
         if isinstance(logs, list):  # Batch logs
@@ -151,9 +190,9 @@ class MetricsLogger(tf.keras.callbacks.Callback):
             batch_start = logs[0]['batch']
             batch_end = logs[-1]['batch']
             if batch_start == batch_end:
-                batch_range = f"{batch_start:04d}_{batch_end:04d}"
-            else:
                 batch_range = f"{batch_start:04d}"
+            else:
+                batch_range = f"{batch_start:04d}_{batch_end:04d}"
             filename = os.path.join(self.get_batch_metrics_savedir(),
                                     f"{stage}_metrics_epoch_{epoch:04d}_batch_{batch_range}.json")
         else:  # Epoch logs
